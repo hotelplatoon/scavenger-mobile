d99c1d7c8ee6e7390050e9b9ef7a668821be4b08
Wynspeare <wynspeare@gmail.com>
2019-04-05 20:16:44 -0500

    User is shown a clue about a  checkpoint + button to prove it

---
+++ scavenger-hunt/components/ClueText.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+// import { ExpoLinksView } from '@expo/samples';
+
+export default class ClueText extends React.Component {
+  // static navigationOptions = {
+  //   title: 'Checkpoint',
+  // };
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.getStartedContainer}>
+            <Text style={styles.subTitleText}>Here is your clue:</Text>
+
+            <Text style={styles.clueText}>{this.props.clueText}</Text>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  subTitleText: {
+    fontSize: 18,
+    color: '#4c0a01',
+    // lineHeight: 30,
+    textAlign: 'center',
+    fontWeight: "500",
+    paddingLeft : 10,
+    paddingRight : 10,
+    paddingTop : 20,
+    marginBottom : 20,
+  },
+  clueText: {
+    fontSize: 15,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+});
--- scavenger-hunt/navigation/AppNavigator.js
+++ scavenger-hunt/navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import ClueScreen from '../screens/ClueScreen'
 
 import MainTabNavigator from './MainTabNavigator';
 
@@ -7,4 +8,5 @@ export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
+  Clue: {screen: ClueScreen}
 }));
