85525cef8c15e298d28350adc2f0ad7047578c1c
crkubiak <crkubiak@gmail.com>
2019-04-12 18:14:14 -0500

    cleaned app, auth, signin, sinup

--- scavenger-hunt/auth.js
+++ scavenger-hunt/auth.js
@@ -1,21 +1,15 @@
 import { AsyncStorage } from "react-native";
 
-// export const USER_KEY = "6a9e885d96c8c68773dae103d6be815236560c3c";
-
-
 export const onSignIn = async (USER_KEY_PROP) => {
-  console.log('****** auth 7',USER_KEY_PROP)
   await AsyncStorage.setItem('USER_KEY', USER_KEY_PROP);
 }
 
 export const onSignOut = async () => await AsyncStorage.removeItem('USER_KEY');
 
 export const isSignedIn = () => {
-  // console.log('****** auth line 14', USER_KEY)
   return new Promise((resolve, reject) => {
     AsyncStorage.getItem('USER_KEY')
       .then(res => {
-        console.log('****** Auth.js 18 res',res)
         if (res !== null) {
           resolve(true);
         } else {
--- scavenger-hunt/screens/SignInScreen.js
+++ scavenger-hunt/screens/SignInScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, AsyncStorage } from "react-native";
+import { View } from "react-native";
 import { Card, Button, Input } from "react-native-elements";
 import { onSignIn } from '../auth'
 import UserAPI from '../api/UserAPI'
@@ -8,7 +8,6 @@ import UserAPI from '../api/UserAPI'
 
 
 export default class SignInScreen extends React.Component {
-// export default ({ navigation }) => (
   constructor(props) {
     super(props)
       this.state = {
@@ -17,65 +16,43 @@ export default class SignInScreen extends React.Component {
       }
     }
 
-  _checkAsync = async () => {
-    let value = await AsyncStorage.getItem('USER_KEY')
-    console.log(value)
-  }
-
-  _handleLogin = () => {
-    console.log('***** 26 SignInScreen this.state.username = ',this.state.username)
-    console.log('***** 27 SignInScreen this.state.password = ',this.state.password)
-    const loginObject =
-    {
-      "username": this.state.username,
-      "password": this.state.password
-    }
+  handleLogin = () => {
+    const loginObject = this.state
     UserAPI.sendLogin(loginObject)
       .then((res) => {
         if (res.status === 200) {
           const USER_KEY = res._bodyInit.slice(9,-1)
-          console.log('********* 38 SignInScren.js',res._bodyInit.slice(9,-1))
           onSignIn(USER_KEY).then(() => this.props.navigation.navigate("SignedInStack"))
-        } else {
-          console.log('false')
         }
       })
-      .catch((error) => {
-        console.log(error)
-      })      
-    }
+      .catch((error) => console.log(error))      
+  }
 
   render() {
     return(
   <View style={{ paddingVertical: 20 }}>
     <Card title="SIGN IN">
+      
       <Input 
         label='Email'
         placeholder='charles@codeplatoon.com...'
         autoCapitalize='none'
         onChangeText={(username) => this.setState({username})}
         />
+      
       <Input
           secureTextEntry
           label='Password'
           placeholder='Password...'
           autoCapitalize='none'
           onChangeText={(password) => this.setState({password})}
-        />      
+      />      
 
       <Button
         buttonStyle={{ marginTop: 20 }}
         backgroundColor="#03A9F4"
         title="SIGN IN"
-        onPress={() => this._handleLogin()}
-        // onPress={() => onSignIn().then(() => navigation.navigate("SignedInStack"))}
-      />
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="#03A9F4"
-        title="Async Test"
-        onPress={() => this._checkAsync()}
-        // onPress={() => onSignIn().then(() => navigation.navigate("SignedInStack"))}
+        onPress={() => this.handleLogin()}
       />
     </Card>
   </View>
