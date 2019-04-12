72893af382783b7b5e8986088f0564f791736284
crkubiak <crkubiak@gmail.com>
2019-04-12 16:07:48 -0500

    login error handling completed

--- scavenger-hunt/screens/SignUpScreen.js
+++ scavenger-hunt/screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextInput, StyleSheet, View, Image} from "react-native";
+import { View } from "react-native";
 import { Button, Card, Input, Icon} from "react-native-elements";
 import UserAPI from '../api/UserAPI'
 
@@ -19,48 +19,31 @@ export default class SignUpScreen extends React.Component {
       allValidatedS: false,
     }
   }
-
-  componentDidUpdate() {
-    console.log(this.state.emailValidated)
-    if (this.state.allValidatedS === false){
-      this.allValidated()
-    } else if (this.state.allValidatedS === false){
-      // this.allValidated()
-    }
-  }
   
   render() {
     return(
-      <View style={styles.container}>
-        <View style={styles.SectionsStyle}>
+      <View>
         <Card title="SIGN UP">
-          <Input
-              style={{flex:1}}
-              label='Name'
-              placeholder='Hunter'
-              onChangeText={this.handleNameChange}
-              value={this.state.name}
-              rightIcon={
-                <Icon
-                name='check'
-                  size={20}
-                  color='green'
-                  
-                  
-                />
-              }
 
-          />
-          {/* <Image source={require('../assets/images/x-mark-3-64.png')} style={styles.ImageStyle} /> */}
- 
           <Input
-            style={{flex:1}}
+            label='Name'
+            placeholder='Hunter'
+            onChangeText={this.handleNameChange}
+            value={this.state.name}
+            rightIcon={ this.state.nameValidated ?  <Icon name='check' color='green' /> : <Icon name='close' color='red' />
+            }
+          />
+
+          <Input
             label='Email'
             placeholder='hunter@scavenger.com...'
             autoCapitalize='none'
-            onChangeText={(text) => this.validateEmail(text)}
+            onChangeText={this.handleEmailChange}
             value={this.state.email}
+            rightIcon={ this.state.emailValidated ?  <Icon name='check' color='green' /> : <Icon name='close' color='red' />
+            }
           />
+
           <Input
             secureTextEntry
             label='Password'
@@ -68,7 +51,10 @@ export default class SignUpScreen extends React.Component {
             autoCapitalize='none'
             onChangeText={this.handlePasswordChange}
             value={this.state.password}
+            rightIcon={ this.state.passwordValidated ?  <Icon name='check' color='green' /> : <Icon name='close' color='red' />
+            }
           />
+
           <Input
             secureTextEntry
             label='Confirm Password'
@@ -76,14 +62,18 @@ export default class SignUpScreen extends React.Component {
             autoCapitalize='none'
             onChangeText={this.handleConfirmPasswordChange}
             value={this.state.confirmPassword}
+            rightIcon={ this.state.confirmPasswordValidated ?  <Icon name='check' color='green' /> : <Icon name='close' color='red' />
+            }
           />
+
           <Button
             buttonStyle={{ marginTop: 20 }}
             backgroundColor="#03A9F4"
             title="SIGN UP"
             onPress={this.addUser}
-            disabled={!this.state.allValidatedS}
+            disabled={!(this.state.nameValidated && this.state.emailValidated && this.state.passwordValidated && this.state.confirmPasswordValidated)}
           />
+
           <Button
             buttonStyle={{ marginTop: 20 }}
             type='outline'
@@ -91,8 +81,8 @@ export default class SignUpScreen extends React.Component {
             title="SIGN IN"
             onPress={this.goSignIn}
           />
+
         </Card>
-      </View>
       </View>
     )
   }
@@ -104,18 +94,15 @@ export default class SignUpScreen extends React.Component {
       this.setState({nameValidated: false})
     }
     this.setState({name})
-    
   }
 
   handlePasswordChange = password => {
     if (password.length > 3) {
       this.setState({passwordValidated: true})
-      
     } else {
       this.setState({passwordValidated: false})
     }
     this.setState({password})
-    
   }
 
   handleConfirmPasswordChange = confirmPassword => {
@@ -126,25 +113,18 @@ export default class SignUpScreen extends React.Component {
     }
     this.setState({confirmPassword})
   }
-  validateEmail = text => {
+
+  handleEmailChange = email => {
     let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ;
-    if(reg.test(text) === false)
+    if(reg.test(email) === false)
     {
-    this.setState({email:text, emailValidated:false})
+    this.setState({email:email, emailValidated:false})
     return false;
       }
     else {
-      this.setState({email:text, emailValidated:true})
+      this.setState({email:email, emailValidated:true})
     }
   }
-
-    allValidated = () => {
-      if (this.state.nameValidated && this.state.emailValidated && this.state.passwordValidated && this.state.confirmPasswordValidated) {
-        this.setState({allValidatedS: true})
-      } else {
-        // this.setState({allValidated:  false})
-      }
-    }
 
   addUser = async () => {
     const user = this.state
@@ -165,51 +145,4 @@ export default class SignUpScreen extends React.Component {
 
   goSignIn = () => this.props.navigation.navigate('SignInScreen')
 
-  // validate = (text) => {
-  //   console.log(text);
-  //   let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
-  //   if(reg.test(text) === false)
-  //   {
-  //   console.log("Email is Not Correct");
-  //   this.setState({email:text, allValidated:true})
-  //   return false;
-  //     }
-  //   else {
-  //     this.setState({email:text, emailValidated:true, allValidated:false})
-
-  //     console.log("Email is Correct");
-  //   }
-  //   }  
 };
-
-const styles = StyleSheet.create({
- 
-  container: {
-    flex: 3,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    margin: 10
-  },
-  
-  SectionStyle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderWidth: .5,
-    borderColor: '#000',
-    height: 40,
-    borderRadius: 5 ,
-    margin: 10
-},
- 
-ImageStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode : 'center',
-    alignItems: 'center'
-},
- 
-});
