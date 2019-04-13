60c1b23d81e868e7befcfb879496501a105e9e8b
TwinklePegasus <ginamariehobbs@gmail.com>
2019-04-13 17:16:40 -0500

    env folder removed. Exit button and game still works successfully.

--- scavenger-hunt/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/gina/Desktop/codePlatoon/team_project/scavenger-mobile/scavenger-hunt/env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
